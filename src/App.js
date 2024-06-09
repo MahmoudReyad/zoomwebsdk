041c4f0015f7eadd6c102d934032e9f999a702e0
Mahmoud Reyad <m.ryad@more-creatives.com>
2024-06-09 21:17:18 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
 
 function App() {
-  useEffect(() => {
-    // Function to call on page load
-    const onPageLoad = () => {
-        console.log('Page is loaded!');
-        // Add your logic here
-    };
-
-    // Call the function on page load
-    onPageLoad();
-}, []);
   const queryParameters = new URLSearchParams(window.location.search)
   const roomId = queryParameters.get("roomId")
   const role = queryParameters.get("role")
